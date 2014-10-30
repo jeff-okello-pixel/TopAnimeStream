01798bf73c183d016785099449f7b8654e8ee78c
matherrien <matherrien@torrentlookup.com>
2014-10-30 13:45:10 -0400

    Asynctask are now executed on a custom executor

--- aniblitz/src/main/java/com/aniblitz/AsyncTaskTools.java
+++ aniblitz/src/main/java/com/aniblitz/AsyncTaskTools.java
@@ -4,6 +4,12 @@ import android.annotation.SuppressLint;
 import android.os.AsyncTask;
 import android.os.Build;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 public class AsyncTaskTools {
     public static <P, T extends AsyncTask<P, ?, ?>> void execute(T task) {
         execute(task, (P[]) null);
@@ -12,7 +18,13 @@ public class AsyncTaskTools {
     @SuppressLint("NewApi")
     public static <P, T extends AsyncTask<P, ?, ?>> void execute(T task, P... params) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-            task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, params);
+            int corePoolSize = 60;
+            int maximumPoolSize = 80;
+            int keepAliveTime = 10;
+
+            BlockingQueue<Runnable> workQueue = new LinkedBlockingQueue<Runnable>(maximumPoolSize);
+            Executor threadPoolExecutor = new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, TimeUnit.SECONDS, workQueue);
+            task.executeOnExecutor(threadPoolExecutor, params);
         } else {
             task.execute(params);
         }
