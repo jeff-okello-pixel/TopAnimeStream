d83f61706cb648a55502b6a8ee401fc5ca50cb1e
marcandre.therrien <matherrien@torrentlookup.com>
2015-02-13 16:31:05 -0500

    add addreviewactivity with layout

---
+++ topanimestream/src/main/java/com/topanimestream/AddReviewActivity.java
@@ -0,0 +1,52 @@
+package com.topanimestream;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.text.Html;
+import android.view.MenuItem;
+import com.topanimestream.managers.AnimationManager;
+
+
+public class AddReviewActivity extends ActionBarActivity {
+
+
+    private SharedPreferences prefs;
+    private int animeId;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.Theme_Blue);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reviews);
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        Intent intent = getIntent();
+        animeId = intent.getExtras().getInt("animeId");
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(false);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setTitle(Html.fromHtml("<font color=#f0f0f0>" + getString(R.string.add_review) + "</font>"));
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                AnimationManager.ActivityFinish(this);
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+        AnimationManager.ActivityFinish(this);
+    }
+
+
+}
