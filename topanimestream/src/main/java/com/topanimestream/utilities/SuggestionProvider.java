2ca94e70f9e353eb1b3c9fcbb65dc0556c318cf1
marcandre.therrien <matherrien@torrentlookup.com>
2015-03-23 13:16:37 -0400

    change utils to utilities

--- topanimestream/src/main/java/com/topanimestream/utilities/SuggestionProvider.java
+++ topanimestream/src/main/java/com/topanimestream/utilities/SuggestionProvider.java
@@ -3,7 +3,7 @@ package com.topanimestream.utilities;
 import android.content.SearchRecentSuggestionsProvider;
 
 public class SuggestionProvider extends SearchRecentSuggestionsProvider {
-    public final static String AUTHORITY = "com.topanimestream.utils.SuggestionProvider";
+    public final static String AUTHORITY = "com.topanimestream.utilities.SuggestionProvider";
     public final static int MODE = DATABASE_MODE_QUERIES;
 
     public SuggestionProvider() {
