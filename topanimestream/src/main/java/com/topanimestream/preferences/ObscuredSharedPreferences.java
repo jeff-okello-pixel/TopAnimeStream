1e4ae0ffa65056a081342ee33089576aa0700c78
marcandre.therrien <matherrien@torrentlookup.com>
2015-05-13 12:48:54 -0400

    update

---
+++ topanimestream/src/main/java/com/topanimestream/preferences/ObscuredSharedPreferences.java
@@ -0,0 +1,7 @@
+package com.topanimestream.preferences;
+
+/**
+ * Created by marcandre.therrien on 2015-05-13.
+ */
+public class ObscuredSharedPreferences {
+}
---
+++ topanimestream/src/main/java/com/topanimestream/utilities/PrefUtils.java
@@ -0,0 +1,7 @@
+package com.topanimestream.utilities;
+
+/**
+ * Created by marcandre.therrien on 2015-05-13.
+ */
+public class PrefUtils {
+}
